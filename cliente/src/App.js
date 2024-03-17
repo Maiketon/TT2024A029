2d60403ee316282429e191e6eeb6629cb21d73d9
Miguel Montoya <miky_lee24@hotmail.com>
2024-03-16 18:40:21 -0600

    3Se agrega en App.js la forma de importar las librerias para utilizarlas, tambien se instala Axios

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import {useState} from "react";
+import Axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css'; //SE IMPORTA BOOSTRAP DENTRO DE REACT//
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 function App() {
   return (
